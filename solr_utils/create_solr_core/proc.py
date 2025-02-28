c8836e84f39ee04151f094e2542d66ecf1fe4baa
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-02-28 17:58:36 +0000

    feat(dev): process output;
    - Print out subprocces output.
    - Use SOLR_HOME envar for configsets dir.

--- solr_utils/create_solr_core/proc.py
+++ solr_utils/create_solr_core/proc.py
@@ -1,8 +1,14 @@
+import os
 import subprocess
 
 def create_solr_core(core_name, config_set):
-    subprocess.run(
+    solr_home = os.environ.get('SOLR_HOME', '/var/solr/data')
+    config_set = f'{solr_home}/configsets/{config_set}'
+    process = subprocess.run(
         ['solr', 'create', '-c', core_name, '-d', config_set],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
+        capture_output=True,
         timeout=60)
+    if process.stdout:
+        print(process.stdout.decode('utf-8'))
+    if process.stderr:
+        print(process.stderr.decode('utf-8'))
