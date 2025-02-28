6f81d3403d82cd357f3b6c428af47269ac0b1fb5
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-02-28 20:38:52 +0000

    feat(dev): callback queue;
    - Added an optional callback function and queue.

--- solr_utils/create_solr_core/proc.py
+++ solr_utils/create_solr_core/proc.py
@@ -1,7 +1,11 @@
 import os
 import subprocess
+import rq
 
-def create_solr_core(core_name, config_set):
+
+def create_solr_core(core_name, config_set,
+                     callback_fn=None, callback_queue=None,
+                     callback_timeout=180):
     solr_home = os.environ.get('SOLR_HOME', '/var/solr/data')
     config_set = f'{solr_home}/configsets/{config_set}'
     process = subprocess.run(
@@ -12,3 +16,18 @@ def create_solr_core(core_name, config_set):
         print(process.stdout.decode('utf-8'))
     if process.stderr:
         print(process.stderr.decode('utf-8'))
+    if callback_fn and callback_queue:
+        current_job = rq.get_current_job()
+        redis_conn = current_job.connection
+        redis_queue = rq.Queue(callback_queue, connection=redis_conn)
+        job = redis_queue.enqueue_call(
+                callback_fn,
+                args=[{'core_name': core_name,
+                       'exit_code': process.returncode,
+                       'stdout': process.stdout.decode('utf-8'),
+                       'stderr': process.stderr.decode('utf-8')}],
+                timeout=callback_timeout)
+        if not job.meta:
+            job.meta = {}
+        job.meta['title'] = 'SOLR Core create callback %s' % core_name
+        job.save()
